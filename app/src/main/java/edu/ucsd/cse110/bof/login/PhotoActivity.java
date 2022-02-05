be60fbaf2f87ece9acb15f7ced9fdaf0ff816b0b
Jason Au <danryan808512@gmail.com>
2022-02-05 15:51:22 -0800

    Updated basic NameActivity and PhotoActivity and their layouts

--- app/src/main/java/edu/ucsd/cse110/bof/login/PhotoActivity.java
+++ app/src/main/java/edu/ucsd/cse110/bof/login/PhotoActivity.java
@@ -12,6 +12,7 @@ import edu.ucsd.cse110.bof.R;
 public class PhotoActivity extends AppCompatActivity {
     private String photoURL;
     private EditText photoInput;
+    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +20,21 @@ public class PhotoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_photo);
 
         photoInput = (EditText)findViewById(R.id.editPhotoURL);
+
+        //Retrieve username sent from NameActivity
+        Bundle extras = getIntent().getExtras();
+        username = extras.getString("name");
     }
 
     public void submitPhoto(View view) {
 
         photoURL = photoInput.getText().toString();
 
-        //TODO: Save the photo
-
-        //Link to InputCourseActivity
+        //TODO: Create a student profile in DB with username and photoURL
 
         /*
+        //Link to InputCourseActivity
         Intent intent = new Intent(this, InputCourseActivity.class);
-
         startActivity(intent);
          */
     }
