b474763392e4ba08b99fe474f9512e17dc0c11bd
Mtam6821 <74935916+Mtam6821@users.noreply.github.com>
2022-02-06 21:31:21 -0800

    made mock listener and UI page

--- app/src/main/java/edu/ucsd/cse110/bof/model/IStudent.java
+++ app/src/main/java/edu/ucsd/cse110/bof/model/IStudent.java
@@ -1,10 +1,11 @@
 package edu.ucsd.cse110.bof.model;
 
+import java.io.Serializable;
 import java.util.List;
 
 import edu.ucsd.cse110.bof.model.db.Course;
 
-public interface IStudent {
+public interface IStudent extends Serializable {
     int getId();
     String getName();
     String getPhotoUrl();
