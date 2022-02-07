39b72447579382b0bc2853127b2bf52bfde8b95e
Allen <aweng@ucsd.edu>
2022-02-06 23:36:21 -0800

    working input activity

--- app/src/main/java/edu/ucsd/cse110/bof/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/bof/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import edu.ucsd.cse110.bof.InputCourses.InputCourseActivity;
 import edu.ucsd.cse110.bof.login.LoginActivity;
 import edu.ucsd.cse110.bof.login.NameActivity;
 
