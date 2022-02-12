0e6b1ced4d2eccb4bc8f8bc876bdf273cf87f37e
Mtam6821 <74935916+Mtam6821@users.noreply.github.com>
2022-02-12 13:16:11 -0800

    homepage started

--- app/src/main/java/edu/ucsd/cse110/bof/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/bof/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         //Test code for starting in Login Activity
         Intent intent = new Intent(this, LoginActivity.class);
         this.startActivity(intent);
---
+++ app/src/main/java/edu/ucsd/cse110/bof/homepage/HomePageActivity.java
@@ -0,0 +1,116 @@
+package edu.ucsd.cse110.bof.homepage;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.ucsd.cse110.bof.BoFsTracker;
+import edu.ucsd.cse110.bof.InputCourses.CoursesViewAdapter;
+import edu.ucsd.cse110.bof.R;
+import edu.ucsd.cse110.bof.model.IStudent;
+import edu.ucsd.cse110.bof.model.db.AppDatabase;
+import edu.ucsd.cse110.bof.model.db.Course;
+import edu.ucsd.cse110.bof.model.db.Student;
+
+public class HomePageActivity extends AppCompatActivity {
+    private AppDatabase db;
+    private IStudent thisStudent;
+    private Context context;
+
+    List<IStudent> students;
+
+    RecyclerView studentsRecyclerView;
+    RecyclerView.LayoutManager studentsLayoutManager;
+    StudentsViewAdapter studentsViewAdapter;
+
+    private static final String TAG = "HomePageReceiver";
+    private MessageListener realListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        setTitle("Birds of a Feather");
+
+        context = this;
+
+        //set thisStudent
+        Intent intent = getIntent();
+        int studentID = intent.getIntExtra("student_id", 0);
+        db = AppDatabase.singleton(context);
+        thisStudent = db.studentsDao().get(studentID);
+
+
+        //set up RecyclerView
+        students = new ArrayList<>();
+
+        studentsRecyclerView = findViewById(R.id.students_view);
+
+        studentsLayoutManager = new LinearLayoutManager(this);
+        studentsRecyclerView.setLayoutManager(studentsLayoutManager);
+
+        studentsViewAdapter = new StudentsViewAdapter(students);
+        studentsRecyclerView.setAdapter(studentsViewAdapter);
+    }
+
+    public void onStartSearchingClicked(View view) {
+        //set up listener for finding BoFs
+        realListener = new MessageListener() {
+            IStudent receivedStudent = null;
+            @Override
+            public void onFound(@NonNull Message message) {
+                //make IStudent from byte array received
+                ByteArrayInputStream bis =
+                        new ByteArrayInputStream(message.getContent());
+                ObjectInput stuObj = null;
+                try {
+                    stuObj = new ObjectInputStream(bis);
+                    receivedStudent = (IStudent) stuObj.readObject();
+                } catch (IOException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                if (!students.contains(receivedStudent)) {
+                    //use BoFsTracker to find common classes
+                    ArrayList<Course> commonCourses = (ArrayList<Course>)
+                            BoFsTracker.getCommonCourses(context, thisStudent,
+                                    receivedStudent);
+
+                    //if not empty list, add this student to list of students
+                    if (commonCourses.size() != 0) {
+                        students.add(receivedStudent);
+
+                        //TODO: sort students by num of commonCourses
+                    }
+                }
+            }
+        };
+
+        Nearby.getMessagesClient(this).subscribe(realListener);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
