fec0d77183dd46c1c0c845bf793515addaa33769
Kevin <k1shi@ucsd.edu>
2022-02-28 14:56:58 -0800

    added priority sorting method stubs

---
+++ app/src/main/java/edu/ucsd/cse110/bof/PrioritySorter.java
@@ -0,0 +1,14 @@
+package edu.ucsd.cse110.bof;
+
+import java.util.List;
+
+import edu.ucsd.cse110.bof.model.db.Student;
+
+public class PrioritySorter {
+
+    public static void sortByRecency(List<Student> students) {
+
+    }
+
+    public static void sortBySize(List<Student> students) {}
+}
