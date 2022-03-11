e3f4d7b76931d2aff7c0ff3c310f6d824a89e5e0
Mtam6821 <74935916+Mtam6821@users.noreply.github.com>
2022-03-11 15:04:19 -0800

    wave unit test updates viewadapter list

--- app/src/test/java/edu/ucsd/cse110/bof/WaveUnitTest.java
+++ app/src/test/java/edu/ucsd/cse110/bof/WaveUnitTest.java
@@ -36,13 +36,17 @@ public class WaveUnitTest {
     private static int userId = 1;
 
     private StudentWithCourses BobAndCourses;
+    private StudentWithCourses CaseyAndCourses;
     private Context context;
 
     private static final String avaUUID = "a4ca50b6-941b-11ec-b9090242ac120002";
     private static final String someUUID1 = "232dc5a5-b428-4ff0-88af-8817afc8e098";
+    private static final String someUUID2 = "7299ef8f-3b21-45d3-b105-f9ceddca48bf";
 
     private static final String bobPhoto = "https://upload.wikimedia" +
             ".org/wikipedia/en/c/c5/Bob_the_builder.jpg";
+    private static final String caseyPhoto = "https://commons.wikimedia" +
+            ".org/wiki/File:Default_pfp.jpg";
 
     //create Ava's courses
     private static final Course cse100FA22S_Ava = new Course(
@@ -79,6 +83,23 @@ public class WaveUnitTest {
             "210",
             "Small");
 
+    private static final Course cse100FA22S_Casey = new Course(
+            courseId++,
+            3,
+            2022,
+            "FA",
+            "CSE",
+            "100",
+            "Small");
+    private static final Course cse110WI22L_Casey = new Course(
+            courseId++,
+            3,
+            2022,
+            "WI",
+            "CSE",
+            "110",
+            "Large");
+
     @Before
     public void createDatabaseAndUser() {
         //Create db
@@ -109,6 +130,20 @@ public class WaveUnitTest {
         db.coursesDao().insert(cse210FA21S_Bob);
 
         BobAndCourses = new StudentWithCourses(Bob, bobCourses, "");
+
+        //add Casey to db
+        Student Casey = new Student("Casey", caseyPhoto, someUUID2);
+        db.studentsDao().insert(Casey);
+
+        List<Course> caseyCourses = new ArrayList<>();
+        caseyCourses.add(cse110WI22L_Casey);
+        caseyCourses.add(cse100FA22S_Casey);
+
+        //add Casey's courses to db
+        db.coursesDao().insert(cse100FA22S_Casey);
+        db.coursesDao().insert(cse110WI22L_Casey);
+
+        CaseyAndCourses = new StudentWithCourses(Casey, caseyCourses, "");
     }
 
     //tests that clicking wave icon updates the db (does not test if ui changes)
@@ -148,11 +183,49 @@ public class WaveUnitTest {
 
             BobAndCourses.setWaveTarget(avaUUID);
 
+            //call listener to update list with Bob waving
             activity.getRealListener().onFound(new Message(studentWithCoursesBytesFactory.convert(BobAndCourses)));
 
             Assert.assertTrue(db.studentsDao().get(2).isWavedAtMe());
+            Assert.assertTrue(activity.getStudentsViewAdapter().getStudents().get(0).equals(BobAndCourses.getStudent()));
         });
     }
 
+    @Test
+    public void testBobWavingIncreasesPriority() {
+        BobAndCourses.setWaveTarget(avaUUID);
 
+        ActivityScenario<HomePageActivity> scenario =
+                ActivityScenario.launch(HomePageActivity.class);
+
+        scenario.moveToState(Lifecycle.State.CREATED);
+
+        scenario.onActivity( activity -> {
+            //use test db
+
+            activity.setMockedStudent(BobAndCourses);
+            activity.setDb(db);
+
+            //click start to add Bob (without wave)
+            activity.onStartSearchingClicked();
+
+            BobAndCourses.setWaveTarget(avaUUID);
+
+            //add Casey to the homepage list
+            activity.getRealListener().onFound(new Message(studentWithCoursesBytesFactory.convert(CaseyAndCourses)));
+
+            //confirm Casey is at top
+            Assert.assertEquals( 2,
+                    activity.getStudentsViewAdapter().getStudents().size());
+            Assert.assertEquals( CaseyAndCourses.getStudent(),
+                    activity.getStudentsViewAdapter().getStudents().get(0));
+
+            //call listener to update list with Bob waving
+            activity.getRealListener().onFound(new Message(studentWithCoursesBytesFactory.convert(BobAndCourses)));
+
+            //confirm Bob is now waving in db and at top of homepage list
+            Assert.assertTrue(db.studentsDao().get(2).isWavedAtMe());
+            Assert.assertEquals( BobAndCourses.getStudent(),
+                    activity.getStudentsViewAdapter().getStudents().get(0));        });
+    }
 }
